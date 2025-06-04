7ea87f4fbd771d916bf30928a7a64feae1edf441
yahui-lee <liyahui@bupt.edu.cn>
2025-06-04 10:48:48 +0800

    Update rigid.py

--- models/deformer/rigid.py
+++ models/deformer/rigid.py
@@ -7,7 +7,7 @@ import pytorch3d.ops as ops
 import trimesh
 import igl
 
-from utils.general_utils import build_rotation
+from utils.general_utils import build_rotation, rotation_matrix_to_quaternion
 from models.network_utils import get_skinning_mlp
 
 class RigidDeform(nn.Module):
@@ -205,7 +205,7 @@ class SkinningField(RigidDeform):
         face_weights = np.mean(self.skinning_weights[self.cano_mesh.faces], axis=1)
 
         hand_joint_indices = [
-        20, 21, 37, 38, 39, 25, 26, 27, 34, 35, 36, 31, 32, 33, 28, 29, 30, 
+        37, 38, 39, 25, 26, 27, 34, 35, 36, 31, 32, 33, 28, 29, 30, 
         52, 53, 54, 40, 41, 42, 49, 50, 51, 46, 47, 48, 43, 44, 45 
         ]
 
@@ -309,8 +309,8 @@ class SkinningField(RigidDeform):
         rotation_hat = build_rotation(gaussians._rotation)
         rotation_bar = torch.matmul(T_fwd[:, :3, :3], rotation_hat)
         setattr(deformed_gaussians, 'rotation_precomp', rotation_bar)
-        # deformed_gaussians._rotation = tf.matrix_to_quaternion(rotation_bar)
-        # deformed_gaussians._rotation = rotation_matrix_to_quaternion(rotation_bar)
+        #deformed_gaussians._rotation = tf.matrix_to_quaternion(rotation_bar)
+        deformed_gaussians._rotation = rotation_matrix_to_quaternion(rotation_bar)
 
         return deformed_gaussians
 
@@ -339,36 +339,3 @@ def get_rigid_deform(cfg, metadata):
         
 #     return joint_positions
 
-# def jointmap(rel_transforms):
-#     import matplotlib.pyplot as plt
-#     from mpl_toolkits.mplot3d import Axes3D
-#     root_position = rel_transforms[0,:,-1].reshape(-1)
-#     joint_positions = compute_joint_positions_relative_to_root(rel_transforms, root_position)
-    
-
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-
-
-#     for i, pos in enumerate(joint_positions):
-#         ax.scatter(pos[0], pos[1], pos[2], color='b', s=50)
-#         ax.text(pos[0], pos[1], pos[2], f'J{i}', color='black')
-
-#     parent_joints = [-1, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 12, 13, 14, 16, 17, 18, 19, 15, 15, 15, 20, 25, 26, 20, 28, 29, 20, 31, 32, 20, 34, 35, 20, 37, 38, 21, 40, 41, 21, 43, 44, 21, 46, 47, 21, 49, 50, 21, 52, 53]
-#     child_joints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54]
-
-
-#     connections = [(parent, child) for parent, child in zip(parent_joints, child_joints) if parent != -1]
-
-#     for (i, j) in connections:
-#         pos_a = joint_positions[i]
-#         pos_b = joint_positions[j]
-#         ax.plot([pos_a[0], pos_b[0]], [pos_a[1], pos_b[1]], [pos_a[2], pos_b[2]], color='r')
-
-
-#     ax.set_xlabel('X')
-#     ax.set_ylabel('Y')
-#     ax.set_zlabel('Z')
-#     ax.set_title('Human Skeleton Structure')
-
-#     plt.show()
